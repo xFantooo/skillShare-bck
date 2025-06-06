941d827b239ad6743bd6066aa9556c35bfe09cda
xFantooo <sulyvane@hotmail.fr>
2025-06-06 10:48:59 +0200

    ajout de la conexion BDD

--- public/index.php
+++ public/index.php
@@ -1,14 +1,25 @@
 <?php
 //Mise en place autoload
 
+use App\core\Database;
 use App\core\Router;
-
 require_once __DIR__ . '/../bootstrap.php';
 
 
-$router = new Router();
 
+
+try {
+    
+$router = new Router();
+$db = Database::getConnexion();
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
 $router->dispatch($uri, $method);
+
+} catch (Exception $e) {
+    return $json = json_encode([
+        'error' => 'Une erreur est survenue',
+        'message' => $e->getMessage()
+    ]);
+}
